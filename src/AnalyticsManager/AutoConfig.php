c7794cbe7790c2da5ec873a7e1c4ca8a930bb88c
Narrakas <netkas@users.noreply.github.com>
2019-06-20 19:51:23 -0400

    Minor correction

--- src/AnalyticsManager/AutoConfig.php
+++ src/AnalyticsManager/AutoConfig.php
@@ -3,6 +3,11 @@
     use acm\acm;
     use acm\Objects\Schema;
 
+    if(class_exists('acm\acm') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
+    }
+
     // Define AC
     $acm = new acm(__DIR__, 'Analytics Manager');
 
